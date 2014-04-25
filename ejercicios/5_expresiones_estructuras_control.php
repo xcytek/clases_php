7d5d238c59cfbbeffb2be5b4cc7cac3bd4f7139a
xcytek <alexcytek@gmail.com>
2014-04-25 19:48:22 +0000

    Ejercicios 5 Expresiones y  Estructuras de control

--- ejercicios/5_expresiones_estructuras_control.php
+++ ejercicios/5_expresiones_estructuras_control.php
@@ -1,7 +1,40 @@
 <?php
 
+// +++++++++++++++++++++++++EJERCICIO 1+++++++++++++++++++++++++++++++++++++++++
+// 
 //Se quiere saber quien de las personas posee la mayor estatura dada en metros. 
 //$aurelio = 1.87
 //$felix = 2.01
 //$marcelo = 1.95
 //Determine quien es mas alto y muestre su estatura en metros, centimetros y pies
+//1m = 3.2808399 pies
+
+
+
+
+// +++++++++++++++++++++++++EJERCICIO 2+++++++++++++++++++++++++++++++++++++++++
+// 
+//Los trabajadores de Anominos Corporation trabajan a la semana en jornadas de 8hrs diarias. El dia se les paga $300 por dia.
+//Acorde a la Ley Federal del Trabajo, si un trabajador labora 6 dias en la semana, el 7mo se le paga.
+//Calcula el sueldo que percibira despues de impuestos segun la siguiente tabla
+//
+//		sueldo		|		%
+//------------------------------
+//		< 900		|		3	
+//		900-1200	|		5
+//		1201-2000	|		8
+//		>2000		|		10
+
+//Determina y muestra:
+//Sueldo Diario
+//Sueldo por Hora
+//Dias Laborados
+//Sueldo Bruto semanal (antes de impuesto)
+//Impuestos y el porcentaje
+//Sueldo Neto Semanal(despues de impuestos).
+//
+//Realiza los Calculos para 3 empleados :
+//
+//trabajador1 laboro 5 dias;
+//trabajador2 laboro 6 dias;
+//trabajador3 laboro 4 dias;
