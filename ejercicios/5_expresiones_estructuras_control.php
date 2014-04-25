67fd70012b2a2aaaf7e72d4da316f1dcb2e04fd8
xcytek <alexcytek@gmail.com>
2014-04-25 18:59:48 +0000

    Ejercicio 5 Expresiones y Estructuras de Control

---
+++ ejercicios/5_expresiones_estructuras_control.php
@@ -0,0 +1,7 @@
+<?php
+
+//Se quiere saber quien de las personas posee la mayor estatura dada en metros. 
+//$aurelio = 1.87
+//$felix = 2.01
+//$marcelo = 1.95
+//Determine quien es mas alto y muestre su estatura en metros, centimetros y pies
